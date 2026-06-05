5d364319a7ffbde3323927303282c8ef8423af66
ViktorFJS24 <vigus006@gmail.com>
2026-06-05 15:54:57 +0200

    Add route for creating api key and for inserting batches (#16)

--- src/lib/supabase/sbProxy.ts
+++ src/lib/supabase/sbProxy.ts
@@ -17,7 +17,8 @@ export async function updateSession(request: NextRequest) {
         !user &&
         !request.nextUrl.pathname.startsWith('/signin') &&
         !request.nextUrl.pathname.startsWith('/signup') &&
-        !request.nextUrl.pathname.startsWith('/api/auth')
+        !request.nextUrl.pathname.startsWith('/api/auth') &&
+        !request.nextUrl.pathname.startsWith('/api/v1')
       ) {
         const url = request.nextUrl.clone();
         url.pathname = '/signin';
