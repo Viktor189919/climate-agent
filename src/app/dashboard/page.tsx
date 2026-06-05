e6469c1250075fd96832c4bc36fde8f655f0b47e
Viktor <vigus006@gmail.com>
2026-06-05 22:26:32 +0200

    Install mui lib for charts

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,36 @@
 'use client'
 
+import { useEffect, useState } from "react";
+import { LineChart } from "@mui/x-charts/LineChart"
+
 export default function Dashboard() {
 
-  
+  const [message, setMessage] = useState<string | null>(null);
+  const [data, setData] = useState(null);
+
+  // useEffect(() => {
+
+  //   async function getData() {
+  //     try {
+  //       const data = fetch('/api/', {
+  //         method: 'GET',
+  //         headers: {
+  //           'Accept': 'application/json'
+  //         }          
+  //       })
+  //     } catch (error) {
+  //       setMessage('Error fetching data');
+  //     }
+  //   }
+
+  //   getData();
+  // })
+
   return (
-    <div></div>
+    <div>
+      <LineChart height={400} width={500} series={[{data: [1,2,3,4,5]}]}>
+
+      </LineChart>
+    </div>
   )
 }
