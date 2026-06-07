e9a7fd08ee8f2b71613c4b0993c648cb54fd68a9
ViktorFJS24 <vigus006@gmail.com>
2026-06-07 16:36:08 +0200

    Install zod and validation on auth routes (#25)

--- src/app/api/auth/signin/route.ts
+++ src/app/api/auth/signin/route.ts
@@ -1,15 +1,21 @@
 import { NextRequest, NextResponse } from "next/server";
-import type { IUserCredentials } from "@/types/auth";
 import { signInWithEmail } from "@/lib/supabase/auth/authentication";
+import { UserCredentials } from "@/lib/zod/schemas";
 
 export async function POST(req: NextRequest) {
 
-  const userData = await req.json() as IUserCredentials;
+  const userData = await req.json();
+
+  const validation = UserCredentials.safeParse(userData);
+
+  if (!validation.success) {
+    return NextResponse.json({message: "Invalid credentials"}, {status: 400});
+  }
 
   const { data, error } = await signInWithEmail(userData);
 
   if (error) {
-    return NextResponse.json({error: error.message, status: error.status});
+    return NextResponse.json({message: error.message, status: error.status});
   }
 
   return NextResponse.json(data);
