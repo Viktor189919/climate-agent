7040e24fbc2aad47e6528a6b330603edbd6b3a07
ViktorFJS24 <vigus006@gmail.com>
2026-06-09 20:06:44 +0200

    Add v1 responses and improve error handling (#26)

--- src/lib/helper/co2Calculator.ts
+++ src/lib/helper/co2Calculator.ts
@@ -1,14 +1,15 @@
-import { AiProvider, WH_PER_PROMPT } from "@/types/calculations";
+import { WH_PER_PROMPT } from "@/types/calculations";
+import { EAiProvider } from "@/types/v1"
 
-export function calculateCO2Emissions(token_count: number, ai_provider: AiProvider, grid_co2_per_kWh_density?: number,): number {
+export function calculateCO2Emissions(token_count: number, ai_provider: EAiProvider, grid_g_co2_per_kWh_density?: number): number {
 
   const globalAvgCo2Density = 300;
-  const gridCo2Density = grid_co2_per_kWh_density ?? globalAvgCo2Density;
+  const gridCo2Density = grid_g_co2_per_kWh_density ?? globalAvgCo2Density;
   const wHperPrompt = WH_PER_PROMPT[ai_provider];
   const tokensPerPrompt = 200;
   const wHperToken = wHperPrompt / tokensPerPrompt;
   const totalWh = token_count * wHperToken;
   const totalKWH = totalWh / 1000;
-  
+
   return totalKWH * gridCo2Density;
 }
