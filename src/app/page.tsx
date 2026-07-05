fbaac45b2e2b4f60cb8785b97010e633f801961e
Viktor <vigus006@gmail.com>
2026-07-05 17:04:49 +0200

    Add header, redirect to dashboard from root page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,7 @@
+import { redirect } from 'next/navigation';
 
-
-export default function Home() {
-  return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-       
-      </main>
-    </div>
-  );
+export default function RootPage() {
+  // Root page is not used, always redirect to dashboard or signin based on user authentication status
+  redirect('/dashboard');
+  return null;
 }
