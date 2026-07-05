4bdb4986327e70bd9f19d9a65c520f5d725bf88d
Viktor <vigus006@gmail.com>
2026-07-05 19:06:16 +0200

    Readd matcher

--- src/proxy.ts
+++ src/proxy.ts
@@ -5,8 +5,8 @@ export async function proxy(request: NextRequest) {
   return await updateSession(request)
 }
 
-// export const config = {
-//   matcher: [
+export const config = {
+  matcher: [
 //     /*
 //      * Match all request paths except for the ones starting with:
 //      * - _next/static (static files)
@@ -14,6 +14,6 @@ export async function proxy(request: NextRequest) {
 //      * - favicon.ico (favicon file)
 //      * Feel free to modify this pattern to include more paths.
 //      */
-//     '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
-//   ],
-// }
+    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+  ],
+}
