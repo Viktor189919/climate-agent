b688aaa84b6bdb2d49e7c200fce938187bdcfb05
Viktor <vigus006@gmail.com>
2026-07-26 21:20:18 +0200

    update styling

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -41,14 +41,23 @@ export default function SignupPage() {
   }
 
   return (
-    <div className="flex flex-col items-center justify-center border-black min-h-screen gap-4">
-      <h1>Sign Up</h1>
-      <div className="flex flex-col gap-2">
-        <input onChange={(e) => setEmail(e.target.value)} placeholder="Email" />
-        <input onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
-        <button className="bg-blue-500 text-white px-4 py-2 rounded" onClick={handleSignup}>Sign Up</button>  
+    <div className="flex min-h-screen flex-col items-center justify-center gap-4 bg-slate-100">
+      <div className="w-80 h-80 border-2 p-10 rounded-lg shadow-lg bg-white">
+        <h1 className="text-3xl font-semibold tracking-widest text-slate-900 transition mb-8">Sign Up</h1>
+        <div className="space-y-4">
+
+          <input className="w-full bg-white border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 p-1 rounded-md" onChange={(e) => setEmail(e.target.value)} placeholder="Email" />
+          <input className="w-full bg-white border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 p-1 rounded-md" onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
+          <button onClick={(e) => {
+            handleSignup();
+          }} className="bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600 transition w-full" >
+            Sign Up
+          </button>
+        </div>
       </div>
-      {message && <p>{message}</p>}
+
+      {message && <p className="text-red-500">{message}</p>}
+
     </div>
   )
 }
