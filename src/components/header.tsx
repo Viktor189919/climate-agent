9f6fed374d0f0e223929bcce85fac3572e4d593c
ViktorFJS24 <vigus006@gmail.com>
2026-08-10 19:31:39 +0200

    Fix rerouting and some response syntax (#37)

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,24 @@
 "use client"
 
 import Link from 'next/link'
+import { useRouter } from 'next/navigation'
 
 export default function Header() {
+  const router = useRouter();
+
+  const handleSignOut = async () => {
+    try {
+      const response = await fetch('/api/auth/signout', { method: 'POST' });
+
+      if (response.ok) {
+        router.push('/signin');
+      }
+
+    } catch (error) {
+      console.error('Error signing out:', error);
+    } 
+  }
+  
   return (
     <header className="border-b border-slate-700 bg-white/80 backdrop-blur-sm">
       <div className="mx-auto flex max-w-8xl items-center justify-between px-4 py-4 sm:px-6 lg:px-8">
@@ -12,7 +28,7 @@ export default function Header() {
 
         <nav className="flex items-center gap-4 text-sm text-slate-600 sm:gap-6">
           
-          <button onClick={() => fetch('/api/auth/signout', { method: 'POST' })} className="transition hover:text-slate-900 hover:cursor-pointer">
+          <button onClick={handleSignOut} className="transition hover:text-slate-900 hover:cursor-pointer">
             Sign Out
           </button>
         </nav>
